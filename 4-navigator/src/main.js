83a8fadeec42dec0b8985e96ecae68dab0a7e10b
ceeriss <komarchuk.nikkom@mail.ru>
2025-11-02 13:33:22 +0300

    dz-2-done

--- 4-navigator/src/main.js
+++ 4-navigator/src/main.js
@@ -1,28 +1,12 @@
-// Задаем координаты
-const addressLat = 10; // адрес назначения lat
-const addressLong = 20; // адрес назначения long
-const positionLat = 5; // текущее положение lat
-const positionLong = 15; // текущее положение long
+let language = prompt("Введите свой язык: ")
 
-// Считаем разницу по координатам
-const diffLat = addressLat - positionLat; // разница по широте
-const diffLong = addressLong - positionLong; // разница по долготе
-
-// Возводим разницы в квадрат
-const diffLatSquared = diffLat * diffLat;
-const diffLongSquared = diffLong * diffLong;
-
-// Складываем квадраты разниц
-const sumOfSquares = diffLatSquared + diffLongSquared;
-
-// Вычисляем квадратный корень простым способом (приблизительно)
-let distance = sumOfSquares / 2; // начинаем с половины числа
-
-// Уточняем результат несколько раз
-distance = (distance + sumOfSquares / distance) / 2;
-distance = (distance + sumOfSquares / distance) / 2;
-distance = (distance + sumOfSquares / distance) / 2;
-
-// Выводим результат
-console.log("Расстояние: " + distance);
-//
+switch (language) {
+	case "en": alert("Hello")
+	break;
+	case "ru": alert("Привет")
+	break;
+	case "de": alert("Gutten Tag")
+	break;
+	default: alert("Unknown language")
+	break;
+}
