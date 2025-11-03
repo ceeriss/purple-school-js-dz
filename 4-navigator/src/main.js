37cf60fd040c9ff2314e4c70332122b94eb9454e
ceeriss <komarchuk.nikkom@mail.ru>
2025-11-03 14:21:36 +0300

    dz-done

--- 4-navigator/src/main.js
+++ 4-navigator/src/main.js
@@ -1,4 +1,15 @@
-const hasLicence = true;
-const age = 18;
-const isDrunk = false;
-console.log(((hasLicence && age >= 18) && !isDrunk) ? 'может' : 'не может');
+function convertor(balance, valut, valutForConvertation){
+	if (valut === 'ру' && valutForConvertation === 'usd') {
+		return (balance = balance / 87);
+	}
+	else if (valut === 'ру' && valutForConvertation === 'eur') {
+		return (balance = balance / 94.5);
+	}
+	else {
+		return null
+	}
+}
+const balance = Number(prompt('Введите баланс в рублях'));
+const valut = prompt('Выберите валюту');
+const valutForConvertation = prompt('Выберите валюту для конвертации');
+alert(convertor(balance, valut, valutForConvertation));
