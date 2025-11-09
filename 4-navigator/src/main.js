9bf1f5c3461bf7f20412424e218ad6d0a2b5229c
ceeriss <komarchuk.nikkom@mail.ru>
2025-11-09 13:38:29 +0300

    dz-done again

--- 4-navigator/src/main.js
+++ 4-navigator/src/main.js
@@ -1,11 +1,10 @@
 const query = {
-    search: 'Вася',
-    take: 10,
+  search: "Вася",
+  take: 10,
+};
+
+function Query(obj, param1, param2) {
+  return `${param1}=${obj[param1]}&${param2}=${obj[param2]}`;
 }
 
-function Query(query, param1, param2) {
-  return `search=${query.param1}&take=${query.param2}`;
-}
-// search=Вася&take=10
-
-console.log(Query(query, 'search', 'take'));
+console.log(Query(query, "search", "take")); // "search=Вася&take=10"
