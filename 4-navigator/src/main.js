b97d240c6022b40b5143c991bd3debe1542a3f9d
ceeriss <komarchuk.nikkom@mail.ru>
2025-11-10 17:50:10 +0300

    dz-done

--- 4-navigator/src/main.js
+++ 4-navigator/src/main.js
@@ -1,10 +1,33 @@
-const query = {
-  search: "Вася",
-  take: 10,
-};
-
-function Query(obj, param1, param2) {
-  return `${param1}=${obj[param1]}&${param2}=${obj[param2]}`;
+const TodoList = {
+	tasks:[],
+	id:0,
+	addTask: function(title, priorety){
+		const task = {
+			id: this.id++,
+			title: title,
+			priorety: priorety,
+		}
+		this.tasks.push(task);
+		return task
+	},
+	removeTask: function(id){
+		this.tasks = this.tasks.filter((task) => task.id !== id)
+	},
+	sortTasks: function () {
+		this.tasks.sort((a,b) => a.priorety - b.priorety);
+	},
+	changeTask: function(id, title, priorety){
+		const task = this.tasks.find((task) => {
+			if (task.id === id) {
+				task.title = title;
+				task.priorety = priorety;
+				return task;
+			}
+		})
+	}
 }
-
-console.log(Query(query, "search", "take")); // "search=Вася&take=10"
+TodoList.addTask("Проверить программу на ошибки", 5);
+TodoList.addTask("gregergreger", 3);
+TodoList.sortTasks()
+TodoList.changeTask(1, 'блаба', 32)
+console.log(TodoList.tasks);
